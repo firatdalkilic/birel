143ac70ecc5ab79ec8c2840bb761b8685118bf48
Fırat Dalkılıç <firatdalkilic87@gmail.com>
2025-07-22 14:45:22 +0300

    MongoDB bağlantısı ve gerekli bağımlılıklar eklendi

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -3,24 +3,20 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error('Lütfen .env.local dosyanıza MONGODB_URI ekleyin.');
+  throw new Error('Please define the MONGODB_URI environment variable inside .env');
 }
 
-type MongooseCache = { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
-const globalWithMongoose = global as typeof globalThis & { mongoose?: MongooseCache };
-const cached: MongooseCache = globalWithMongoose.mongoose || { conn: null, promise: null };
-
-export async function dbConnect() {
-  if (cached.conn) return cached.conn;
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
+async function dbConnect() {
+  try {
+    const opts = {
       bufferCommands: false,
-    }).then((mongoose) => mongoose);
+    };
+
+    const db = await mongoose.connect(MONGODB_URI, opts);
+    return db;
+  } catch (error) {
+    throw error;
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
 
-if (typeof window === 'undefined') {
-  globalWithMongoose.mongoose = cached;
-} 
+export default dbConnect; 
