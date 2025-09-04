becf726c2ca3c5495a109ca601ec51726c38dabc
Fırat Dalkılıç <firatdalkilic87@gmail.com>
2025-09-04 03:47:27 +0300

    fix: tailwind config - add pages and components paths for Next 13/14

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
     darkMode: ['class'],
     content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
