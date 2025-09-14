232819abe71ba94f60fecf91f851e79055782ba1
birel-ci <root@srv.birelapp.com>
2025-09-15 01:37:01 +0300

    update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: "birel",
+      name: "birel-web",
       cwd: "/var/www/birel",
       script: ".next/standalone/server.js",
-      args: "-p 3000",
       exec_mode: "fork",
       instances: 1,
-      env: {
-        NODE_ENV: "production",
-        PORT: "3000"
-      }
+      env: { NODE_ENV: "production", PORT: "3000" }
     }
   ]
 };
